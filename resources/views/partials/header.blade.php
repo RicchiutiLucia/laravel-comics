573c97c123dc6b3c314992ae32e57db1d916a71a
RicchiutiLucia <luciaricchiuti04@gmail.com>
2023-05-10 16:43:32 +0200

    header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -3,16 +3,15 @@
         <div class="img-container">
             <a href="{{ route('home') }}"><img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="dc logo"></a>
         </div>
-
         <ul>
             @foreach (config('comics.header_links') as $link)
                 <li>
+                    
                     <a href="{{ $link['route'] }}">
                         {{ $link['title'] }}
                     </a>
                 </li>
             @endforeach
-
         </ul>
     </nav>
 </header>
