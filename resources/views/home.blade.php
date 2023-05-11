b2cb61c53e7f4b4d81c62002defdd543f620a64b
RicchiutiLucia <luciaricchiuti04@gmail.com>
2023-05-11 12:28:52 +0200

    esercizio base concluso

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,5 +3,19 @@
 @section('page-title', 'Home page')
 
 @section('content')
-    <p>Qui ci sarà il contenuto della pagina</p>
+    <div class="container">
+        <div class="row">
+            @foreach($comics as $comic)
+                <div class="col-3 h-75 ">
+                    <div class="card m-5 bg-dark " >
+                    <img src="{{$comic['thumb']}}" class="card-img-top ">
+                    <div class="card-body">
+                        <h5 class="card-title text-uppercase fs-6">{{$comic['title']}}</h5>
+                        
+                    </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
 @endsection
